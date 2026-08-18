abbe4db7dbb195344d04abb3f6c8a65b29c9e6a4
rouniyarkritika4-lang <rouniyarkritika4@gmail.com>
2026-08-18 22:49:14 +0545

    Streamlit added for backend API

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import streamlit as st
 
 API_BASE = os.getenv(
     "API_BASE",
-    "https://movie-rec-466x.onrender.com"
+    "https://movie-recommendation-system-t1c6.onrender.com" or "https://movie-rec-466x.onrender.com" 
 ).rstrip("/")
 
 TMDB_IMG = "https://image.tmdb.org/t/p/w500"
